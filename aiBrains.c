3def48d1ea7c4e4c6c22ba399a8c40713cd65cdb
VSjnk <100817326+VSjnk@users.noreply.github.com>
2023-11-17 11:03:27 -0800

    Update aiBrains.c
    Gave Micheal a bit more brains

--- aiBrains.c
+++ aiBrains.c
@@ -3,18 +3,31 @@
 //---Texture Stuff---
 #include "textures/Micheal.c"
 
+int playerX=0;
+int playerY=0;
+extern int playerDir=0;
+int speed=1;
+
 typedef struct
 {
 	int x,y;
 	int frame;
 }Demon; Demon De;
 
+void Fight(int Cast)
+{
+playerDir=Cast;
+}
+
+
 void Micheal()
 {	
 	De.frame += 1;
-	De.x = 0;
-	De.y = 0;
-	
+	if(De.x <= playerX){De.x +=speed;}else{De.x -=speed;}
+	if(De.y <= playerY-1){De.y +=speed;}else if(De.y >= playerY+1){De.y -=speed;}
+	if(De.x == playerX && De.y == playerY){dead(4);}
+	if(KEY_A && playerDir==0){speed= -1;}
+	if(De.x == 0 | De.x == 120){speed=1;}
 	if(De.frame == 1)
 	{
 		mirrorImage(12, 10, Micheal_Map, 0, De.x, De.y);
@@ -26,9 +39,11 @@ void Micheal()
 }
 
 
-int summonDemon()
+int summonDemon(int PX, int PY)
 {
 	Micheal();
+	playerX = PX;
+	playerY = PY;
 }
 
 
